bbdc55d7418f7e9db5ac3559984b4ed00036bfe9
michelgm <99933941+michelgm@users.noreply.github.com>
2024-07-11 08:59:29 -0300

    Criando tratativas de erro

--- lib/imc.dart
+++ lib/imc.dart
@@ -4,5 +4,11 @@ void main() {
 }
 
 double calcIMC(double altura, double peso) {
+  if (altura < 1) {
+    throw Exception('Altura não pode ser menor que 1');
+  }
+  if (peso < 1) {
+    throw Exception('Peso não pode ser menor que 1');
+  }
   return peso / (altura * altura);
 }
