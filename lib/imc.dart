409ce6c55db87e39c026bea5896e413cc4e6c19e
michelgm <99933941+michelgm@users.noreply.github.com>
2024-07-11 09:15:33 -0300

    Criando API propria

--- lib/imc.dart
+++ lib/imc.dart
@@ -1,6 +1,25 @@
+import 'dart:math';
+
 void main() {
-  final result = calcIMC(1.78, 64);
-  print(result);
+  if (calcIMC(1.78, 64) == 20.199469763918696) {
+    print('OK: Deve efetuar o calculo do IMC');
+  } else {
+    print('ERRO: Deve efetuar o calculo do IMC');
+  }
+
+  try {
+    calcIMC(0, 64);
+    print('ERROR: Deve lançar uma excessão se altura for menor que 1');
+  } catch (e) {
+    print('OK: Deve lançar uma excessão se altura for menor que 1');
+  }
+
+  try {
+    calcIMC(1.78, 0);
+    print('ERROR: Deve lançar uma excessão se peso for menor que 1');
+  } catch (e) {
+    print('OK: Deve lançar uma excessão se peso for menor que 1');
+  }
 }
 
 double calcIMC(double altura, double peso) {
@@ -10,5 +29,5 @@ double calcIMC(double altura, double peso) {
   if (peso < 1) {
     throw Exception('Peso não pode ser menor que 1');
   }
-  return peso / (altura * altura);
+  return peso / pow(altura, 2);
 }
