05ced332e469073b21451b3ba85f8f7767045286
michelgm <99933941+michelgm@users.noreply.github.com>
2024-07-15 20:25:07 -0300

    api de teste dart

---
+++ test/imc_test.dart
@@ -0,0 +1,17 @@
+import 'package:dart_tests/imc.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  test('Deve efetuar o calculo do IMC', () {
+    final result = calcIMC(1.78, 64);
+    expect(result, equals(20.199469763918696));
+  });
+
+  test('Deve lançar uma excessão se altura for menor que 1', () {
+    expect(() => calcIMC(0, 64), throwsA(isA<Exception>()));
+  });
+  test('Deve lançar uma excessão se peso for menor que 1', () {
+    expect(() => calcIMC(1.78, 0), throwsA(isA<Exception>()));
+  });
+}
