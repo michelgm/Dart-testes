004969e23004ad434354481c52dfdf9fcb067cd4
michelgm <99933941+michelgm@users.noreply.github.com>
2024-07-17 09:53:35 -0300

    adicionando grupos

--- test/imc_test.dart
+++ test/imc_test.dart
@@ -8,10 +8,12 @@ void main() {
     expect(result, equals(20.199469763918696));
   });
 
-  test('Deve lançar uma excessão se altura for menor que 1', () {
-    expect(() => calcIMC(0, 64), throwsA(isA<Exception>()));
-  });
-  test('Deve lançar uma excessão se peso for menor que 1', () {
-    expect(() => calcIMC(1.78, 0), throwsA(isA<Exception>()));
+  group('Excessões de parametros |', () {
+    test('Deve lançar uma excessão se altura for menor que 1', () {
+      expect(() => calcIMC(0, 64), throwsA(isA<Exception>()));
+    });
+    test('Deve lançar uma excessão se peso for menor que 1', () {
+      expect(() => calcIMC(1.78, 0), throwsA(isA<Exception>()));
+    });
   });
 }
